318415390c18fa6f05048bc768e77c65c82d428f
Unni P <unnipofficial@outlook.com>
2026-08-18 11:46:56 +0530

    fixed ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iamunnip.github.io',
   output: 'static',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
